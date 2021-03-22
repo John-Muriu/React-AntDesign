4c5758d53f312285520d4ebf7826cf98b5b23384
John Muriu <johnmuriw@gmail.com>
2021-03-22 14:43:29 +0300

    update

--- src/App.js
+++ src/App.js
@@ -1,8 +1,7 @@
 // import logo from './logo.svg';
 import './App.css';
-import 'antd/dist/antd.css'; // or 'antd/dist/antd.less'
-
-// import { Button } from 'antd';
+import 'antd/dist/antd.css'; // or 'antd/dist/antd.
+import AppHeader from './components/common/header'
 import { Layout, Menu } from 'antd';
 
 const { Header, Content, Footer } = Layout;
@@ -12,6 +11,7 @@ function App() {
   return (
     <Layout className="layout">
       <Header>
+        <AppHeader />
         <div className="logo" />
         <Menu theme="dark" mode="horizontal" defaultSelectedKeys={['2']}>
           <Menu.Item key="1">nav 1</Menu.Item>
