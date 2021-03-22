75ee03df45c63dd2d3812dd20a33c3b55e7a22a7
John Muriu <johnmuriw@gmail.com>
2021-03-22 14:28:18 +0300

    update

--- src/App.js
+++ src/App.js
@@ -1,30 +1,28 @@
-import logo from './logo.svg';
+// import logo from './logo.svg';
 import './App.css';
 import 'antd/dist/antd.css'; // or 'antd/dist/antd.less'
 
-import { Button } from 'antd';
+// import { Button } from 'antd';
+import { Layout, Menu } from 'antd';
+
+const { Header, Content, Footer } = Layout;
+
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-      <Button type="primary">Primary Button</Button>
-
-    </div>
+    <Layout className="layout">
+      <Header>
+        <div className="logo" />
+        <Menu theme="dark" mode="horizontal" defaultSelectedKeys={['2']}>
+          <Menu.Item key="1">nav 1</Menu.Item>
+          <Menu.Item key="2">nav 2</Menu.Item>
+          <Menu.Item key="3">nav 3</Menu.Item>
+        </Menu>
+      </Header>
+    </Layout>
+    // mountNode,
   );
+
 }
 
 export default App;
