ac8bd226735bb4a1b302c61c2bd892df842665ee
John Muriu <johnmuriw@gmail.com>
2021-03-23 12:31:50 +0300

    about us section

---
+++ src/components/home/about.js
@@ -0,0 +1,28 @@
+import React from 'react'
+
+
+const items = [{
+    key: '1',
+    title: 'Mobile developers',
+    content: 'development company providing a complete software design and development service. JohnT',
+},
+{
+    key: '2',
+    title: 'software development company ',
+    content: 'class custom software solutions, elite software development teams and innovative cloud software to enterprise business across numerous industries',
+},
+{
+    key: '3',
+    title: ' JohnTech delivers the be',
+    content: 'enterprise business across numerous industries',
+}
+
+
+]
+function AppAbout() {
+    return (
+        
+    )
+}
+
+export default AppAbout;
