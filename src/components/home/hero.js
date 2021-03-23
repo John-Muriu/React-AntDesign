017123c3570d264dc0aec7116cf85fb4ff2c2ee2
John Muriu <johnmuriw@gmail.com>
2021-03-23 11:15:33 +0300

    update

--- src/components/home/hero.js
+++ src/components/home/hero.js
@@ -1,9 +1,28 @@
 import React from 'react';
+import { Carousel } from 'antd';
+
+
+const itemsv = v[]
 
 function AppHero() {
     return (
-        <h1> hero black
-        </h1>
+        <div className="heroBlock">
+
+            <Carousel>
+                <div>
+                    <h3 >1</h3>
+                </div>
+                <div>
+                    <h3 >2</h3>
+                </div>
+                <div>
+                    <h3>3</h3>
+                </div>
+                <div>
+                    <h3 >4</h3>
+                </div>
+            </Carousel>
+        </div>
     )
 }
 
