1753f1af77ae46be9725597267daa28999a713bc
John Muriu <johnmuriw@gmail.com>
2021-03-26 16:46:21 +0300

    minor changes

--- src/components/common/header.js
+++ src/components/common/header.js
@@ -1,6 +1,6 @@
 import React from 'react';
 import { Menu } from 'antd';
-// import { Anchor } from 'antd'
+import { Anchor } from 'antd'
 
 function AppHeader() {
     return (
@@ -10,18 +10,18 @@ function AppHeader() {
                     <i className="fas fa-bolt"></i>
                     <a rel="noopener noreferrer" href="http://www.github.com/john-muriu" target="_blank">JohnTech</a>
                 </div>
-                {/* <Anchor targetOffset="65"> */}
-                <Menu mode="horizontal" defaultSelectedKeys={['home']}>
-                    {/* <div className="mobileHidden">  </div> */}
-                    <Menu.Item key="home">Home</Menu.Item>
-                    <Menu.Item key="about">About</Menu.Item>
-                    <Menu.Item key="features">Features</Menu.Item>
-                    <Menu.Item key="howitworks">How It works</Menu.Item>
-                    <Menu.Item key="faq">FAQ</Menu.Item>
-                    <Menu.Item key="pricing">Pricing</Menu.Item>
-                    <Menu.Item key="contact">Contacts</Menu.Item>
-                </Menu>
-                {/* </Anchor> */}
+                <Anchor targetOffset="65">
+                    <Menu mode="horizontal" defaultSelectedKeys={['home']}>
+                        <div className="mobileHidden">  </div>
+                        <Menu.Item key="home">Home</Menu.Item>
+                        <Menu.Item key="about">About</Menu.Item>
+                        <Menu.Item key="features">Features</Menu.Item>
+                        <Menu.Item key="howitworks">How It works</Menu.Item>
+                        <Menu.Item key="faq">FAQ</Menu.Item>
+                        <Menu.Item key="pricing">Pricing</Menu.Item>
+                        <Menu.Item key="contact">Contacts</Menu.Item>
+                    </Menu>
+                </Anchor>
 
             </div>
         </div>
