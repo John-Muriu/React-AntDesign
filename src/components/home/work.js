a17b3f2f53ec8f5be001468dba6f470b7ef0affc
John Muriu <johnmuriw@gmail.com>
2021-03-26 15:25:46 +0300

    work.js

--- src/components/home/work.js
+++ src/components/home/work.js
@@ -1,35 +1,38 @@
 import React from 'react'
 import { Modal, Button } from 'antd'
-import useState from 'react'
+import { useState } from 'react'
 
 const AppWork = () => {
-    const [showModal, isModalVisible, handleOk, handleCancel] = useState(false)
+    const [isModalVisible, setIsModalVisible] = useState(false);
 
+    const showModal = () => {
+        setIsModalVisible(true);
+    };
 
-
-
+    const handleCancel = () => {
+        setIsModalVisible(false);
+    };
 
     return (
-        <div className="block worksBlock ">
+        <div className="block worksBlock">
             <div className="container-fluid">
                 <div className="titleHolder">
                     <h2> How It Works</h2>
-                    <p>Lorem ipsum dolor sit amet consectetur adipisicing elit.</p>
-
+                    <p>Lorem ipsum dolor sit amet.</p>
                 </div>
-                <Button type="primary" onClick={showModal}>
-                    Open Modal
-      </Button>
-                <Modal title="Basic Modal" visible={isModalVisible} onOk={handleOk} onCancel={handleCancel}>
-                    <p>Some contents...</p>
-                    <iframe title="MyLife" width="100%" height="350" src="https://www.youtube.com/watch?v=PEDXYcQTwd0" frameborder="0"></iframe>
-                </Modal>
+            </div>
+            <div className="contentHolder">
+                <Button onClick={showModal}>
+                    <i className="fas fa-play"></i>
+                </Button>
 
             </div>
+            <Modal title="HowItWorks" visible={isModalVisible} onCancel={handleCancel} footer={null}>
+                <iframe width="100%" height="405" src="https://www.youtube.com/embed/rwjmyZUA2O8" title="YouTube video player" frameborder="0" allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture" allowfullscreen></iframe>
+            </Modal>
         </div>
     );
-}
-
+};
 
 export default AppWork
 
