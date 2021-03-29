4cda4e446bebe145643d8fcf64cca9ff84ec94fc
John Muriu <johnmuriw@gmail.com>
2021-03-29 21:26:38 +0300

    nnu

--- src/components/home/pricing.js
+++ src/components/home/pricing.js
@@ -45,28 +45,34 @@ const data = [
 function AppPricing() {
 
     return (
-        <div>
-            <List
-                grid={{
-                    gutter: 16,
-                    xs: 1,
-                    sm: 2,
-                    md: 4,
-                    lg: 4,
-                    xl: 6,
-                    xxl: 3,
-                }}
-                dataSource={data}
-                renderItem={item => (
-                    <List.Item>
-                        <Card title={item.title}>
-                            <p>{item.content}</p>
-                        </Card>
-                    </List.Item>
-                )}
-            />,
+        <div className="block pricingBlock bgGray">
+            <div className="container-fluid">
+                <List
+                    grid={{
+                        gutter: 16,
+                        xs: 1,
+                        sm: 2,
+                        md: 4,
+                        lg: 4,
+                        xl: 6,
+                        xxl: 3,
+                    }}
+                    dataSource={data}
+                    renderItem={item => (
+                        <List.Item>
+                            <Card title={item.title}>
+                                <p>{item.content[0].price}</p>
+                                <p>{item.content[0].space}</p>
+                                <p>{item.content[0].user}</p>
+                                <p>{item.content[0].support}</p>
+                                <p>{item.content[0].backup}</p>
+                                <p>{item.content[0].access}</p>
+                            </Card>
+                        </List.Item>
+                    )}
+                />,
 
-
+</div>
         </div>
     )
 }
